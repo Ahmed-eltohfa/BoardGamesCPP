b743f129afb25d8c5266d1d3724d5bd5ddc52864
Mostafa205dev <seso63412@gmail.com>
2024-11-30 15:05:22 +0200

    add problem 5

--- Assignment workstation/main.cpp
+++ Assignment workstation/main.cpp
@@ -9,6 +9,9 @@
 #include "Classes/FiveXFive/FiveXFive.cpp"
 #include "Classes/WordsTicTacToe/WordsTicTacToe.h"
 #include "Classes/WordsTicTacToe/WordsTicTacToe.cpp"
+#include "Classes/NumericalTicTacToe/NumericalTicTacToe.h"
+#include "Classes/NumericalTicTacToe/NumericalTicTacToe.cpp"
+
 #include <algorithm> // For std::max and std::min
 
 void pyramidGameMainRun()
@@ -221,15 +224,75 @@ void FourInRowGame(){
     GameManager<char> FourInRowGameManager(B,players);
     FourInRowGameManager.run();
 
+    // Clean up
+    delete B;
+    for (int i = 0; i < 2; ++i)
+    {
+        delete players[i];
+    }
 };
 
 
+void NumericalTicTacToeGame(){
 
+    int choice;
+    string player1Name, player2Name;
+    Player<int>* players[2];
+    Numerical_Tic_Tac_Toe_Board<int>* B = new Numerical_Tic_Tac_Toe_Board<int>();
+
+    cout << "Choose Player X type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cout << "3. Smart Computer (AI)\n";
+    cin >> choice;
+
+    if(choice == 1)
+    {
+        // Set up player 1
+        cout << "Enter Player X name: ";
+        cin >> player1Name;
+        players[0]= new Numerical_Tic_Tac_Toe_Player<int>(player1Name, 1);
+
+    }else if(choice ==2)
+    {
+        players[0]= new Numerical_Tic_Tac_Toe_Random_Player<int>(1);
+    }else{
+        cout << "Invalid choice for Player 1. Exiting the game.\n";
+        return;
+    }
+
+
+
+    cout << "Choose Player 2 type:\n";
+    cout << "1. Human\n";
+    cout << "2. Random Computer\n";
+    cout << "3. Smart Computer (AI)\n";
+    cin >> choice;
+    
+    if(choice == 1)
+    {
+        // Set up player 2
+        cout << "Enter Player 2 name: ";
+        cin >> player2Name;
+        players[1]= new Numerical_Tic_Tac_Toe_Player<int>(player2Name, 1);
+    }else if(choice ==2)
+    {
+        players[1]= new Numerical_Tic_Tac_Toe_Random_Player<int>(1);
+    }else{
+        cout << "Invalid choice for Player 1. Exiting the game.\n";
+        return;
+    }
+
+    GameManager<int> NumericalTicTacToeGameManager(B,players);
+    NumericalTicTacToeGameManager.run();
+
+    
+}
 using namespace std;
 int main()
 {
     // WordsGameMainRun();
-    // return 0;
-    FourInRowGame();
+    // FourInRowGame();
+    NumericalTicTacToeGame();
     
 }
