6d8fc94f9d39f5d79bd3a0c543ed9463b5263c26
Mostafa205dev <seso63412@gmail.com>
2024-12-21 12:43:55 +0200

    new change

--- Assignment workstation/Classes/FourInRow/FourInRow.cpp
+++ Assignment workstation/Classes/FourInRow/FourInRow.cpp
@@ -162,7 +162,7 @@ Four_In_Row_Player<T>::Four_In_Row_Player(string name, T symbol) : Player<T>(nam
 template <typename T>
 void Four_In_Row_Player<T>::getmove(int &x, int &y)
 {
-    cout << "\nenter the coloum of coloum u want\n";
+    cout << "\nenter the number of column u want\n";
     cin >> y;
 }
 /////////////////////////////////////// end player IMPLEMENTATION
